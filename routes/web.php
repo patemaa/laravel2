8a5580ad4f48e61a66f6a08f32e5b103fcc9f518
Fatma R <anthoschichequs@gmail.com>
2025-01-22 11:19:46 +0300

    form icin client side ve server side validasyonlar eklendi. button'lar icin ayri bir dosya olusturuldu.

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,11 @@ Route::get('jobs/create', function () {
     return view('jobs.create');
 });
 Route::post('/jobs', function () {
+    request()->validate([
+        'title' => ['required', 'min:3'],
+        'salary' => ['required']
+    ]);
+
     Job::create([
         'title' => request('title'),
         'salary' => request('salary'),
