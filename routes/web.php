922f1022a5067546cf1129f1f763fdde11eb8e1a
Fatma R <anthoschichequs@gmail.com>
2025-01-24 12:39:08 +0300

    baska bir test route eklendi. ->queue

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,14 @@
 use App\Http\Controllers\JobController;
 use App\Http\Controllers\RegisteredUserController;
 use App\Http\Controllers\SessionController;
+use App\Jobs\TranslateJob;
+use App\Models\Job;
 use Illuminate\Support\Facades\Route;
 
 Route::get('test', function () {
-    dispatch(function () {
-        logger('hello form the queue!');
-    });
+    $job = Job::first();
+
+    TranslateJob::dispatch($job);
 
     return 'Done!';
 });
